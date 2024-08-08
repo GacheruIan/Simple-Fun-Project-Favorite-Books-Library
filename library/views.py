5b0824933927e730d0f390165d15583311bc8bf6
ian gacheru <gacheruian99@gmail.com>
2024-08-08 14:32:44 +0300

    library app under:construction:

---
+++ library/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+
+# Create your views here.
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'library/book_list.html', {'books': books})
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'library/book_detail.html', {'book': book})
